103eb1a4d67200bb958f2cc35f57f5836d137fff
manjinder22fzk <manjindersinghfzk@gmail.com>
2025-12-02 11:07:57 -0500

    Continuous Deployment Code

--- infra/terraform/envs/dev/variables.tf
+++ infra/terraform/envs/dev/variables.tf
@@ -4,11 +4,11 @@ variable "region" {
   default     = "us-east-1"
 }
 
-variable "aws_profile" {
-  description = "AWS CLI profile for dev environment"
-  type        = string
-  default     = "dev"
-}
+# variable "aws_profile" {
+#   description = "AWS CLI profile for dev environment"
+#   type        = string
+#   default     = "dev"
+# }
 
 variable "lambda_package_path" {
   description = "Local path to the Lambda zip for dev"
